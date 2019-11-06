9ae51320e4277ce1133f3a1c70e504409ce9a750
Emiliya <emiliya@Emiliyas-MacBook-Pro.local>
2019-11-05 19:18:29 -0500

    routers setup

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 const express = require('express')
 const connectDB = require('./config/db')
 const app = express();
+const users = require('./routes/api/users')
+const auth = require('./routes/api/auth')
+const profile = require('./routes/api/profile')
+const posts = require('./routes/api/posts')
 
-
+//Connect DB
 connectDB();
+
 app.get("/", (req, res) => res.send("Hello"))
+
+// Define Routes
+app.use("/api/users", users)
+app.use("/api/auth", auth)
+app.use("/api/profile", profile)
+app.use("/api/posts", posts)
+
 
 const PORT = process.env.PORT || 5000;
 

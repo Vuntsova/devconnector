087291723b47fb2c752fe155b8dc6316ff539475
Emiliya <emiliya@Emiliyas-MacBook-Pro.local>
2019-11-07 16:12:48 -0500

    gravatar, bcrypt and jsonwebtoken

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,10 +1,77 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const { check, validationResult } = require('express-validator');
+const User = require('../../models/User');
 
+// @route    POST api/users
+// @desc     Register user
+// @access   Public
+router.post(
+  '/',
+  [
+    check('name', 'Name is required')
+      .not()
+      .isEmpty(),
+    check('email', 'Email is reqiored').isEmail(),
+    check('password').isLength({ min: 6 })
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
 
-// @route   GET api/users
-// @desc    Test route
-// @access  Public
-router.get('/', (req, res) => res.send("User Route"))
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
 
-module.exports = router
+    const { name, email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+
+      if (user) {
+        return res.status(400).json({ errors: [{ msg: 'User ecists' }] });
+      }
+
+      const avatar = gravatar.url(email, {
+        s: '200',
+        r: 'pg',
+        d: 'mm'
+      });
+
+      user = new User({
+        name,
+        email,
+        avatar,
+        password
+      });
+
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+      jwt.sign(
+        payload,
+        config.get('jwtSercret'),
+        { expiresIn: '1h' },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error');
+    }
+  }
+);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,23 +1,23 @@
-const express = require('express')
-const connectDB = require('./config/db')
+const express = require('express');
+const connectDB = require('./config/db');
 const app = express();
-const users = require('./routes/api/users')
-const auth = require('./routes/api/auth')
-const profile = require('./routes/api/profile')
-const posts = require('./routes/api/posts')
+const users = require('./routes/api/users');
+const auth = require('./routes/api/auth');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
 
 //Connect DB
 connectDB();
-
-app.get("/", (req, res) => res.send("Hello"))
+//Init Middleware
+app.use(express.json({ extended: false }));
+app.get('/', (req, res) => res.send('Hello'));
 
 // Define Routes
-app.use("/api/users", users)
-app.use("/api/auth", auth)
-app.use("/api/profile", profile)
-app.use("/api/posts", posts)
-
+app.use('/api/users', users);
+app.use('/api/auth', auth);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server Runs on Port: ${PORT}`))
+app.listen(PORT, () => console.log(`Server Runs on Port: ${PORT}`));
